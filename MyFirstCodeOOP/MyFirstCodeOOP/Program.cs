2df402ffc9004c5860ab50961fc702399b75b0ba
ALEJANDRA CARVAJAL CAMPO <alejandracarvajal316210@correo.itm.edu.co>
2023-02-15 14:29:18 -0500

    I created my first code with a new Date Class

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MyFirtsCodeOPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingrese el año: ");
+                int year = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el mes: ");
+                int month = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el día: ");
+                int day = int.Parse(Console.ReadLine());
+
+                var dateObject = new Date(year, month, day);
+                Console.WriteLine(dateObject);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
